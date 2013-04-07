718307b24447dbca8ded5e96830e8cd018225112
Chenyang Yuan <yuanchenyang@gmail.com>
2013-04-07 12:53:50 -0700

    Finished demos

--- demo.scm
+++ demo.scm
@@ -1,18 +1,29 @@
-;; Arithmatic
+;;; Arithmatic
 (+ (* 4 5) (/ 10 2))
 
-;; Assignment
+;;; Assignment
 (define x 6)
 (+ x x)
+;; 12
 
-;; Lists
+;;; Lists
+(define x (cons 1 (cons '(2 4) (cons 3 nil))))
+x
+;; (1 (2 4) 3)
+(car (cdr x))
+;; (2 4)
 
+;;; Lambda functions
+(define y (lambda (x) (* x x)))
+(y (y 2))
+;; 16
 
-;; Lambda functions
-
-;; Higher order functions
+;;; Higher order functions
 (define f
   (lambda (x)
     (lambda (y)
       (+ x y))))
 (f 4)
+;; <procedure>
+((f 3) 2)
+;; 5
