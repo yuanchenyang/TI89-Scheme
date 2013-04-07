0ce1dd4c0908eff454d974569471b29c97f518e2
Chenyang Yuan <yuanchenyang@gmail.com>
2013-04-06 19:54:57 -0700

    Started tokenizer

--- interpreter.c
+++ interpreter.c
@@ -167,11 +167,11 @@ _main() {
   printf("\n");
   printToken(cdr(cdr(car(t0))));
   printf("\n");
-  Token *tp = tokenize("(begin (+ 2 3) (+ 5 6))");
+  Token *tp = tokenize("(define (enumerate-tree tree)(cond ((null? tree) nil)((not (pair? tree)) (list tree))(else (append (enumerate-tree (car tree))(enumerate-tree (cdr tree))))))(enumerate-tree (list 1 (list 2 (list 3 4)) 5))");
   while ((*tp).type != T_END) {
     printToken(*tp);
     printf(" ");
     tp++;
-  }
+  }  
   ngetchx();
 }
